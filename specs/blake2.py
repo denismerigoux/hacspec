806a3b5991a9d9bfa928751dc674ff5e6a7e4eed
Denis Merigoux <denis.merigoux@gmail.com>
2018-04-27 17:49:32 +0200

    Better style for blake2 spec

--- specs/blake2.py
+++ specs/blake2.py
@@ -1,78 +1,8 @@
 from speclib import *
 
-variant = refine3(nat_t, lambda x: x == 0 or x == 1)
-
-
-def blake2(v:variant):
-    if v == 1:
-        bits_in_word = 64
-        rounds_in_f = 12
-        block_bytes = 128
-        R1 = 32
-        R2 = 24
-        R3 = 16
-        R4 = 63
-        working_vector_t = array_t(uint64_t, 16)
-        hash_vector_t = array_t(uint64_t, 8)
-        index_t = range_t(0, 16)
-        IV = array([
-            uint64(0x6A09E667F3BCC908), uint64(0xBB67AE8584CAA73B),
-            uint64(0x3C6EF372FE94F82B), uint64(0xA54FF53A5F1D36F1),
-            uint64(0x510E527FADE682D1), uint64(0x9B05688C2B3E6C1F),
-            uint64(0x1F83D9ABFB41BD6B), uint64(0x5BE0CD19137E2179)
-        ])
-        to_word = uint64
-        word_t = uint64_t
-        minus_one = uint64(0xFFFFFFFFFFFFFFFF)
-        data_internal_t = refine(bytes_t, lambda x: array.length(
-            x) < 2 ** 64 and (array.length(x) % block_bytes == 0))
-        key_t = refine(vlbytes_t, lambda x: array.length(x) <= 64)
-        key_size_t = refine(nat, lambda x: x <= 64)
-        out_size_t = refine(nat, lambda x: x <= 32)
-        to_words_le = vlbytes.to_uint64s_le
-        from_words_le = vlbytes.from_uint64s_le
-        low_bits = to_word
-        high_bits = lambda x: to_word(x >> 64)
-        to_double_word = uint128
-        max_size_t = 2**64 - 1
-        data_t = refine(vlbytes_t, lambda x: vlbytes.lenght(x)
-                            < max_size_t - 2 * block_bytes)
-    else:
-        bits_in_word = 32
-        rounds_in_f = 10
-        block_bytes = 64
-        R1 = 16
-        R2 = 12
-        R3 = 8
-        R4 = 7
-        working_vector_t = array_t(uint32_t, 16)
-        hash_vector_t = array_t(uint32_t, 8)
-        index_t = range_t(0, 16)
-        IV = array([
-            uint32(0x6A09E667), uint32(0xBB67AE85),
-            uint32(0x3C6EF372), uint32(0xA54FF53A),
-            uint32(0x510E527F), uint32(0x9B05688C),
-            uint32(0x1F83D9AB), uint32(0x5BE0CD19)
-        ])
-        to_word = uint32
-        word_t = uint32_t
-        minus_one = uint32(0xFFFFFFFF)
-        data_internal_t = refine(bytes_t, lambda x: array.length(
-            x) < 2 ** 64 and (array.length(x) % block_bytes == 0))
-        key_t = refine(vlbytes_t, lambda x: array.length(x) <= 32)
-        key_size_t = refine(nat, lambda x: x <= 32)
-        out_size_t = refine(nat, lambda x: x <= 32)
-        to_words_le = vlbytes.to_uint32s_le
-        from_words_le = vlbytes.from_uint32s_le
-        low_bits = to_word
-        high_bits = lambda x: to_word(x >> 32)
-        to_double_word = uint64
-        max_size_t = 2**32 - 1
-        data_t = refine(vlbytes_t, lambda x: vlbytes.lenght(x)
-                        < max_size_t - 2 * block_bytes)
-
-
-    SIGMA: array_t(index_t, 16 * 12) = array([
+variant = refine(nat, lambda x: x == 0 or x == 1)
+index_t = range_t(0,16)
+_SIGMA: array_t(index_t, 16 * 12) = array([
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
         14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3,
         11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4,
@@ -87,79 +17,215 @@ def blake2(v:variant):
         14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3
     ])
 
+def highbits_128(x:uint128_t):
+    return uint64(x >> 64)
+def highbits_64(x:uint64_t):
+    return uint32(x >> 32)
+def word_t(v:variant):
+    if v == 0:
+        ty = uint64_t
+    else:
+        ty = uint32_t
+    return ty
 
-    def G(v: working_vector_t, a: index_t, b: index_t, c: index_t, d: index_t, x: uint64_t, y: uint64_t) -> working_vector_t:
+def double_word_t(v:variant):
+    if v == 0:
+        ty = uint128_t
+    else:
+        ty = uint64_t
+    return ty
+
+def word_bits(v:variant):
+    if v == 0:
+        b = 64
+    else:
+        b = 32
+    return b
+
+def to_word(v:variant):
+    if v == 0:
+        f = uint64
+    else:
+        f = uint32
+    return f
+
+def to_words_le(v:variant):
+    if v == 0:
+        f = vlbytes.to_uint64s_le
+    else:
+        f = vlbytes.to_uint32s_le
+    return f
+
+def from_words_le(v:variant):
+    if v == 0:
+        f = vlbytes.from_uint64s_le
+    else:
+        f = vlbytes.from_uint32s_le
+    return f
+
+def rounds_in_f(v:variant):
+    if v == 0:
+        r =  12
+    else:
+        r = 10
+    return r
+
+def block_bytes(v:variant):
+    if v == 0:
+        bb = 128
+    else:
+        bb = 64
+    return bb
+
+def _R(v:variant):
+    if v == 0:
+        t = (32,24,16,63)
+    else:
+        t = (16,12,8,7)
+    return t
+
+def _IV(v:variant):
+    if v == 0:
+        iv = array([
+            uint64(0x6A09E667F3BCC908), uint64(0xBB67AE8584CAA73B),
+            uint64(0x3C6EF372FE94F82B), uint64(0xA54FF53A5F1D36F1),
+            uint64(0x510E527FADE682D1), uint64(0x9B05688C2B3E6C1F),
+            uint64(0x1F83D9ABFB41BD6B), uint64(0x5BE0CD19137E2179)
+        ])
+    else:
+        iv = array([
+            uint32(0x6A09E667), uint32(0xBB67AE85),
+            uint32(0x3C6EF372), uint32(0xA54FF53A),
+            uint32(0x510E527F), uint32(0x9B05688C),
+            uint32(0x1F83D9AB), uint32(0x5BE0CD19)
+        ])
+    return iv
+
+
+def high_bits(v:variant):
+    if v == 0:
+        f = highbits_128
+    else:
+        f = highbits_64
+    return f
+
+def to_double_word(v:variant):
+    if v == 0:
+        f = uint128
+    else:
+        f = uint64
+    return f
+
+def working_vector_t(v:variant):
+    return array_t(word_t(v),16)
+
+def hash_vector_t(v:variant):
+    return array_t(word_t(v), 8)
+
+def max_size_t(v:variant):
+    return (2 ** word_bits(v) - 1)
+
+def minus_one(v:variant):
+    return (to_word(v))(max_size_t(v))
+
+def data_internal_t(v:variant):
+    return refine(bytes_t,
+                  lambda x: array.length(x) < 2 ** word_bits(v)
+                            and (array.length(x) % block_bytes(v) == 0))
+
+def key_t(v:variant):
+    return refine(vlbytes_t, lambda x: array.length(x) <= word_bits(v))
+
+def key_size_t(v:variant):
+    return refine(nat, lambda x: x <= word_bits(v))
+
+def out_size_t(v:variant):
+    return refine(nat, lambda x: x <= word_bits(v))
+
+
+def low_bits(v:variant):
+    return to_word(v)
+
+def data_t(v:variant):
+    return refine(vlbytes_t, lambda x: vlbytes.length(x)
+                        < max_size_t(v) - 2 * block_bytes(v))
+
+def blake2(var:variant):
+    def _G(v: working_vector_t(var), a: index_t, b: index_t, c: index_t, d: index_t, x: word_t(var), y: word_t(var)) -> working_vector_t(var):
+        (_R1,_R2,_R3,_R4) = _R(var)
         v[a] = v[a] + v[b] + x
-        v[d] = word_t.rotate_right(v[d] ^ v[a], R1)
+        v[d] = word_t(var).rotate_right(v[d] ^ v[a], _R1)
         v[c] = v[c] + v[d]
-        v[b] = word_t.rotate_right(v[b] ^ v[c], R2)
+        v[b] = word_t(var).rotate_right(v[b] ^ v[c], _R2)
         v[a] = v[a] + v[b] + y
-        v[d] = word_t.rotate_right(v[d] ^ v[a], R3)
+        v[d] = word_t(var).rotate_right(v[d] ^ v[a], _R3)
         v[c] = v[c] + v[d]
-        v[b] = word_t.rotate_right(v[b] ^ v[c], R4)
+        v[b] = word_t(var).rotate_right(v[b] ^ v[c], _R4)
         return v
 
-
-    def F(h: hash_vector_t, m: working_vector_t, t: uint128_t, flag: bool) -> hash_vector_t:
-        v = array.create(16, to_word(0))
+    def _F(h: hash_vector_t(var), m: working_vector_t(var), t: double_word_t(var), flag: bool) -> hash_vector_t(var):
+        v = array.create(16, to_word(var)(0))
         v[0:8] = h
-        v[8:16] = IV
-        v[12] = v[12] ^ low_bits(t)
-        v[13] = v[13] ^ high_bits(t >> 64)
+        v[8:16] = _IV(var)
+        v[12] = v[12] ^ low_bits(var)(t)
+        v[13] = v[13] ^ high_bits(var)(t >> 64)
         if flag == True:
-            v[14] = v[14] ^ minus_one
-        for i in range(rounds_in_f):
-            s = SIGMA[i * 16:(i + 1) * 16]
-            v = G(v, 0, 4, 8, 12, m[s[0]], m[s[1]])
-            v = G(v, 1, 5, 9, 13, m[s[2]], m[s[3]])
-            v = G(v, 2, 6, 10, 14, m[s[4]], m[s[5]])
-            v = G(v, 3, 7, 11, 15, m[s[6]], m[s[7]])
-            v = G(v, 0, 5, 10, 15, m[s[8]], m[s[9]])
-            v = G(v, 1, 6, 11, 12, m[s[10]], m[s[11]])
-            v = G(v, 2, 7, 8, 13, m[s[12]], m[s[13]])
-            v = G(v, 3, 4, 9, 14, m[s[14]], m[s[15]])
+            v[14] = v[14] ^ minus_one(var)
+        for i in range(rounds_in_f(var)):
+            s = _SIGMA[i * 16:(i + 1) * 16]
+            v = _G(v, 0, 4, 8, 12, m[s[0]], m[s[1]])
+            v = _G(v, 1, 5, 9, 13, m[s[2]], m[s[3]])
+            v = _G(v, 2, 6, 10, 14, m[s[4]], m[s[5]])
+            v = _G(v, 3, 7, 11, 15, m[s[6]], m[s[7]])
+            v = _G(v, 0, 5, 10, 15, m[s[8]], m[s[9]])
+            v = _G(v, 1, 6, 11, 12, m[s[10]], m[s[11]])
+            v = _G(v, 2, 7, 8, 13, m[s[12]], m[s[13]])
+            v = _G(v, 3, 4, 9, 14, m[s[14]], m[s[15]])
         for i in range(8):
             h[i] = h[i] ^ v[i] ^ v[i + 8]
         return h
 
-
-    def blake2_internal(data: data_internal_t, input_bytes: uint128_t, kk: key_size_t, nn: out_size_t) \
+    def blake2_internal(data: data_internal_t(var),
+               input_bytes: uint128_t,
+               kk: key_size_t(var),
+               nn: out_size_t) \
             -> contract(vlbytes_t,
                         lambda data, input_bytes, kk, nn: True,
                         lambda data, input_bytes, kk, nn, res: array.length(res) == nn):
-        h = array.copy(IV)
-        h[0] = h[0] ^ to_word(0x01010000) ^ (to_word(kk) << 8) ^ to_word(nn)
-        data_blocks = array.length(data) // block_bytes
+        h = array.copy(_IV(var))
+        h[0] = h[0] ^ to_word(var)(0x01010000) ^ (to_word(var)(kk) << 8) ^ to_word(var)(nn)
+        bb = block_bytes(var)
+        data_blocks = array.length(data) // bb
         if data_blocks > 1:
             for i in range(data_blocks - 1):
-                h = F(h, to_words_le(
-                    data[block_bytes * i:block_bytes * (i + 1)]), to_double_word((i + 1) * block_bytes), False)
+                h = _F(h, to_words_le(var)(
+                    data[bb * i:bb * (i + 1)]), to_double_word(var)((i + 1) * bb), False)
         if kk == 0:
-            h = F(h, to_words_le(
-                data[block_bytes * (data_blocks - 1):block_bytes * data_blocks]), to_double_word(input_bytes), True)
+            h = _F(h, to_words_le(var)(
+                data[bb * (data_blocks - 1):bb * data_blocks]), to_double_word(var)(input_bytes), True)
         else:
-            h = F(h, to_words_le(
-                data[block_bytes * (data_blocks - 1):block_bytes * data_blocks]), to_double_word(input_bytes + block_bytes), True)
-        return from_words_le(h)[:nn]
+            h = _F(h, to_words_le(var)(
+                data[bb * (data_blocks - 1):bb * data_blocks]), to_double_word(var)(input_bytes + bb), True)
+        return from_words_le(var)(h)[:nn]
 
-
-    def blake2(data: data_t, key: key_t, nn: out_size_t) \
+    def blake2(data: data_t(var), key: key_t(var), nn: out_size_t(var)) \
             -> contract(vlbytes_t,
-                        lambda data, key, nn: True, lambda data, key, nn, res: array.length(res) == nn):
+                        lambda data, key, nn: True,
+                        lambda data, key, nn, res: array.length(res) == nn):
         ll = array.length(data)
         kk = array.length(key)
-        data_blocks = (ll - 1) // block_bytes + 1
-        padded_data_length = data_blocks * block_bytes
+        bb = block_bytes(var)
+        data_blocks = (ll - 1) // bb + 1
+        padded_data_length = data_blocks * bb
         if kk == 0:
             padded_data = array.create(padded_data_length, uint8(0))
             padded_data[:ll] = data
         else:
-            padded_data = array.create(padded_data_length + block_bytes, uint8(0))
+            padded_data = array.create(padded_data_length + bb, uint8(0))
             padded_data[0:kk] = key
-            padded_data[block_bytes:block_bytes+ll] = key
+            padded_data[bb:bb+ll] = key
         return blake2_internal(padded_data, ll, kk, nn)
-
     return blake2
 
-blake2s = blake2(variant(0))
-blake2b = blake2(variant(1))
+blake2s = blake2(variant(1))
+blake2b = blake2(variant(0))
